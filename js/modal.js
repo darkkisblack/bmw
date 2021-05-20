30b143085dfc8477a0cc2bab711d35410327d064
darkkisblack <70438354+darkkisblack@users.noreply.github.com>
2021-05-20 21:20:18 +0700

    second commmit

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const designElem = document.querySelector('.design-text');
+const modalElem = document.querySelector('.modal');
+
+const openModal = () => {
+    modalElem.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modalElem.classList.add('hidden');
+}
+
+designElem.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('more')) openModal();
+})
+
+modalElem.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('overlay') || 
+        target.classList.contains('modal__close')) {
+            closeModal()
+        }
+})
